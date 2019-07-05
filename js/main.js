0431f8ab397fef923c5774d1bcd204fcea084674
carcamcue <carcamcue@gmail.com>
2019-07-05 14:01:07 +0200

    Beta 0.1.8

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* Variables configuración */
 var url_base = window.location.href.includes("localhost") || window.location.href.includes("127.0.0.1") ? "/opensdg/" : "/";
-var version_app = "0.1.7";
+var version_app = "0.1.8";
 
 /* Variables globales */
 var my_chart;
